4b1bf3d67b8ab8f88abc1cfa67e88bb2109b88ca
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2026-04-26 10:31:02 -0700

    Update Scoreboard.java

--- src/main/java/Scoreboard.java
+++ src/main/java/Scoreboard.java
@@ -1,16 +1,24 @@
 public class Scoreboard {
+    boolean team1Active = true;
+    int team1Score = 0;
+    int team2Score = 0;
 
+    String team1;
+    String team2;
 
     public Scoreboard(String team1, String team2) {
-
+        this.team1 = team1;
+        this.team2 = team2;
     }
 
     public void recordPlay(int score) {
-
+        if(score == 0) team1Active = !team1Active;
+        else if(team1Active) team1 += score;
+        else team2 += score;
     }
 
     public String getScore() {
-        return "";
+        return team1Score + "-" + team2Score + "-" + team1Active ? team1 : team2;
     }
 
 }
