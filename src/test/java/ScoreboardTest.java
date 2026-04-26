116138b5946dfa9a11b79d2ddce86df54a27086f
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2026-04-26 10:39:44 -0700

    fix the tests

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -9,7 +9,7 @@ public class ScoreboardTest {
         scoreboard.recordPlay(0);
         scoreboard.recordPlay(3);
 
-        String expected = "1-3 Team B";
+        String expected = "1-3-Team B";
         String message = "Check Scoreboard recordPlay() and getScore()";
         assertEquals(message,  expected, scoreboard.getScore());
     }
@@ -21,7 +21,7 @@ public class ScoreboardTest {
         scoreboard.recordPlay(0);
         scoreboard.recordPlay(3);
 
-        String notExpected = "1-3 Team A";
+        String notExpected = "1-3-Team A";
         String message = "Check Scoreboard recordPlay() and getScore()";
         assertTrue(message, !scoreboard.getScore().equals(notExpected));
     }
@@ -30,6 +30,6 @@ public class ScoreboardTest {
     public void testScoreboardInitial() {
         Scoreboard scoreboard = new Scoreboard("A", "B");
 
-        assertEquals("Check Scoreboard recordPlay() and getScore()", scoreboard.getScore(), "0-0 A");
+        assertEquals("Check Scoreboard recordPlay() and getScore()", scoreboard.getScore(), "0-0-A");
     }
 }
