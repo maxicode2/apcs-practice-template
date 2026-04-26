635cffc76d2e3834add6e47401ebff9e0de43cbb
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2026-04-26 10:58:28 -0700

    make tests

---
+++ src/test/java/WordCheckerTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WordCheckerTest {
+    @Test(timeout = 1000)
+    public void test1() {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("catch");
+        words.add("bobcat");
+        words.add("catchacat");
+        words.add("cat");
+        words.add("at");
+        WordChecker wc = new WordChecker(words);
+
+        
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("ch");
+        expected.add("chacat");
+        expected.add("");
+        assertEquals("oops", expected , wc.createList("cat"));
+    }
+
+  @Test(timeout = 1000)
+    public void test2() {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        WordChecker wc = new WordChecker(words);
+
+        
+        assertTrue("oops" , wc.isWordChain());
+    }
+}
