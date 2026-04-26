8f4112866eff08c884c57d50dae2fd7f42f7e5d5
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2026-04-26 10:34:15 -0700

    Update ScoreboardTest.java
    Added a second test case to validate scoreboard functionality.

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -2,7 +2,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ScoreboardTest {
-    
     @Test(timeout = 1000)
     public void testScoreboardPlay1() {
         Scoreboard scoreboard = new Scoreboard("Team A", "Team B");
@@ -15,6 +14,21 @@ public class ScoreboardTest {
         assertEquals(message,  expected, scoreboard.getScore());
     }
 
-    // TODO: Create another test case 
+    @Test(timeout = 1000)
+    public void testScoreboardPlay2() {
+        Scoreboard scoreboard = new Scoreboard("Team A", "Team B");
+        scoreboard.recordPlay(1);
+        scoreboard.recordPlay(0);
+        scoreboard.recordPlay(3);
 
+        String notExpected = "1-3 Team A";
+        String message = "Check Scoreboard recordPlay() and getScore()";
+        assertTrue(message, !scoreboard.getScore().equals(expected));
+    }
+
+    @Test(timeout = 1000)
+    public void testScoreboardInitial() {
+        Scoreboard scoreboard = new Scoreboard("A", "B");
+
+        assertEquals("Check Scoreboard recordPlay() and getScore()", scoreboard.getScore(), "0-0 A");
 }
